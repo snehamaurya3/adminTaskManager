2eaea502a2e4c78434cee0c61850ba44fc8c25bc
snehamaurya3 <mauryasneha80@gmail.com>
2026-05-01 12:18:44 +0530

    fix register route

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
   if (err) {
     console.error("DB Error:", err);
-    return;
+  } else {
+    console.log("MySQL Connected ✅");
   }
-  console.log("✅ MySQL Connected");
 });
 
 module.exports = db;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,19 @@ const router = require("express").Router();
 const { register, login } = require("../controllers/authController");
 const { verifyToken, isAdmin } = require("../middleware/authMiddleware");
 
-// router.post("/register", verifyToken, isAdmin, register);
 router.post("/register", register);
+router.post("/create", verifyToken, isAdmin, register);
 router.post("/login", login);
 
 module.exports = router;
+
+
+// const router = require("express").Router();
+// const { register, login } = require("../controllers/authController");
+// const { verifyToken, isAdmin } = require("../middleware/authMiddleware");
+
+// // router.post("/register", verifyToken, isAdmin, register);
+// router.post("/register", register);
+// router.post("/login", login);
+
+// module.exports = router;
