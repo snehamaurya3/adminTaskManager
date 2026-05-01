02a2d7af1e6ff8f347fc742bfa239ef93dd105ad
snehamaurya3 <mauryasneha80@gmail.com>
2026-05-01 12:13:40 +0530

    fix register route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { register, login } = require("../controllers/authController");
 const { verifyToken, isAdmin } = require("../middleware/authMiddleware");
 
-router.post("/register", verifyToken, isAdmin, register);
+// router.post("/register", verifyToken, isAdmin, register);
 router.post("/register", register);
 router.post("/login", login);
 
