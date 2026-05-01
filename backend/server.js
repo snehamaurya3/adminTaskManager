593e7dd4772da5a0985299049ad505784c51169e
snehamaurya3 <mauryasneha80@gmail.com>
2026-05-01 11:25:21 +0530

    connecting with backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,13 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+
+
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 // app.listen(5000, () => {
--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://admintaskmanager-production.up.railway.app/",
 });
 
 export default API;
